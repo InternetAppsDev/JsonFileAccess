e1b9a600546264541a5582a5b17f66dd1aba164f
Damien Costello <dcostello@hotmail.com>
2018-11-28 14:14:06 +0000

    Add project files.

---
+++ FileAccessSample/FileAccessSample/Dogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileAccessSample
+{
+    public class Dogs
+    {
+        // breed, category, origin, 
+        // grooming, exercise, picture
+        public string Breed { get; set; }
+        public string Category { get; set; }
+        public string Origin { get; set; }
+        public string Grooming { get; set; }
+        public string Exercise { get; set; }
+        public string Picture { get; set; }
+    }
+}
---
+++ FileAccessSample/FileAccessSample/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+namespace FileAccessSample
+{
+    public partial class MainPage : ContentPage
+    {
+        // global list to store the data
+        // for CRUD
+        List<Dogs> dogs;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            SetUpLists();
+            // set the dogs list as the source for the list view.
+            lvDogs.ItemsSource = dogs;
+        }
+
+        private void SetUpLists()
+        {
+            if (dogs != null) return;
+            //list is not created, instantiate and fill
+            dogs = new List<Dogs>();
+            // need a link to the assembly (dll) to get the file
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+            // create a stream to access the file
+            Stream stream = assembly.GetManifestResourceStream(
+                                "FileAccessSample.Data.DogsJson.txt");
+            // create a stream reader to read from the stream
+            using (var reader = new StreamReader(stream))
+            {
+                string jsonText = reader.ReadToEnd();
+                // use a Json parser to deserialize the text in file
+                // to a list of object of type Dogs
+                dogs = JsonConvert.DeserializeObject<List<Dogs>>(jsonText);
+
+            }
+        }
+
+        private void lvDogs_ItemSelected(object sender, 
+                                    SelectedItemChangedEventArgs e)
+        {
+            Dogs current = (Dogs)e.SelectedItem;
+        }
+    }
+}
