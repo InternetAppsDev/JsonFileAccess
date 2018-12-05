7acf90371294443100a81ad8d23b0c785b01c647
Damien Costello <dcostello@hotmail.com>
2018-12-05 18:34:21 +0000

    Added the reading and writing to files, completed the sample

--- FileAccessSample/FileAccessSample/MainPage.xaml.cs
+++ FileAccessSample/FileAccessSample/MainPage.xaml.cs
@@ -16,39 +16,108 @@ namespace FileAccessSample
         // for CRUD
         List<Dogs> dogs;
 
+        // == constant values ==
+        private const string OUTPUT_FILE = "JsonDogs.txt";
+
         public MainPage()
         {
             InitializeComponent();
-            SetUpLists();
-            // set the dogs list as the source for the list view.
-            lvDogs.ItemsSource = dogs;
+            if (dogs == null) dogs = new List<Dogs>();
+            SetUpDogsLists();
+            RefreshDogsListView();
         }
 
-        private void SetUpLists()
+        private void SetUpDogsLists()
         {
-            if (dogs != null) return;
-            //list is not created, instantiate and fill
-            dogs = new List<Dogs>();
-            // need a link to the assembly (dll) to get the file
-            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
-            // create a stream to access the file
-            Stream stream = assembly.GetManifestResourceStream(
-                                "FileAccessSample.Data.DogsJson.txt");
-            // create a stream reader to read from the stream
-            using (var reader = new StreamReader(stream))
+            try
             {
-                string jsonText = reader.ReadToEnd();
-                // use a Json parser to deserialize the text in file
-                // to a list of object of type Dogs
-                dogs = JsonConvert.DeserializeObject<List<Dogs>>(jsonText);
-
+                // read the data to a local file
+                string path = Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData);
+                string filename = Path.Combine(path, OUTPUT_FILE);
+                using (var reader = new StreamReader(filename))
+                {
+                    string jsonText = reader.ReadToEnd();
+                    // use a Json parser to deserialize the text in file
+                    // to a list of object of type Dogs
+                    dogs = JsonConvert.DeserializeObject<List<Dogs>>(jsonText);
+                }
             }
+            catch
+            {
+                // need a link to the assembly (dll) to get the file
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+                // create a stream to access the file
+                Stream stream = assembly.GetManifestResourceStream(
+                                    "FileAccessSample.Data.DogsJson.txt");
+                // create a stream reader to read from the stream
+                using (var reader = new StreamReader(stream))
+                {
+                    string jsonText = reader.ReadToEnd();
+                    // use a Json parser to deserialize the text in file
+                    // to a list of object of type Dogs
+                    dogs = JsonConvert.DeserializeObject<List<Dogs>>(jsonText);
+                }
+            } // end try catch
         }
 
         private void lvDogs_ItemSelected(object sender, 
                                     SelectedItemChangedEventArgs e)
         {
             Dogs current = (Dogs)e.SelectedItem;
+            slOneDog.BindingContext = current;
+            // need to set the button to be disable as setting the 
+            // data through the BindingContext causes a TextChanged
+            // event to fire on the Entry boxes
+            btnUpdate.IsEnabled = false;
+            slOneDog.IsVisible = true;
+        }
+
+        private void btnSave_Clicked(object sender, EventArgs e)
+        {
+            // save the data to a local file
+            string path = Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData);
+            string filename = Path.Combine(path, OUTPUT_FILE);
+
+            using (var streamWriter = new StreamWriter(filename, false))
+            {
+                string jsonText = JsonConvert.SerializeObject(dogs);
+                streamWriter.WriteLine(jsonText);
+            }
+        }
+
+        private void btnRead_Clicked(object sender, EventArgs e)
+        {
+            SetUpDogsLists();
+            RefreshDogsListView();
+        }
+
+        private void OneDogEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btnUpdate.IsEnabled = true;
+        }
+
+        private void btnUpdate_Clicked(object sender, EventArgs e)
+        {
+
+            foreach (var dog in dogs)
+            {
+                if (lblBreed.Text == dog.Breed)
+                {
+                    dog.Category = lblCategory.Text;
+                    dog.Origin = lblOrigin.Text;
+                    dog.Grooming = lblGrooming.Text;
+                    dog.Exercise = lblExercise.Text;
+                }
+            }
+            RefreshDogsListView();
+        }
+
+        private void RefreshDogsListView()
+        {
+            lvDogs.ItemsSource = null;
+            lvDogs.ItemsSource = dogs;
         }
     }
 }
